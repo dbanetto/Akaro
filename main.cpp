581aa15019fbc217ad158a0298800d748de6b63a
David Barnett <david@barnett.net.nz>
2013-10-16 17:25:31 +1300

    Update main * Removed unneeded includes * using toColour() for background colour

--- main.cpp
+++ main.cpp
@@ -5,19 +5,14 @@
 */
 
 #include "GameWindow.h"
-#include <iostream>
-#include <string>
-#include "io/file.h"
+#include "etc/colour.h"
 
 int main (int argc, char* argv[]) 
 {
     GameWindow gm;
-    //Background Colour
-    SDL_Color bg;
-    bg.r = 0; bg.g = 0; bg.b = 0; 
 
     //Make sure the window init'ed properly
-    if ( gm.init ( "Akaro" , bg, SDL_WINDOW_OPENGL ) == 0 )
+    if ( gm.init ( "Akaro" , etc::toColour(etc::COLOUR_BLACK) , SDL_WINDOW_OPENGL ) == 0 )
     {
         //14 - Start the game already!
         gm.start();
