95e053a6f7c1a352c35b996e5adad4b26c47b61b
David Barnett <david@barnett.net.nz>
2013-10-18 23:31:08 +1300

    Inverse Z Score + inverse z score function * Comments and todo updated

--- math/distnorm.cpp
+++ math/distnorm.cpp
@@ -11,9 +11,15 @@ namespace math
 {
   double dist_norm_zscore (double x , double mean, double sd )
   {
-    // (X - x) / s.d
+    // (X - x) / s.d = Z
     // Z Score equation
     return (x - mean) / sd;
+  }
+
+  double dist_norm_inverse_zscore ( double z,  double mean, double sd )
+  {
+    //  Z * s.d. + x = X
+    return (sd * z) + mean;
   }
 
 } /* namespace math */
--- math/distnorm.h
+++ math/distnorm.h
@@ -14,8 +14,10 @@ namespace math
 {
   //TODO : Add probability (double between 0 and 1) to Z Score function
   //TODO : Add a Z score to value (with given s.d. and mean)
+  //TODO : give better names to this file
 
   double dist_norm_zscore ( double x,  double mean, double sd );
+  double dist_norm_inverse_zscore ( double z,  double mean, double sd );
 
 } /* namespace math */
 
