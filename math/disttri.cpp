34c90c42719755e2f14dad8927db1dfcade83925
David Barnett <david@barnett.net.nz>
2013-10-18 23:32:05 +1300

    Tri rnd * Basic outline of dist_tri_rnd is commented in * Updated ToDo

--- math/disttri.cpp
+++ math/disttri.cpp
@@ -6,8 +6,21 @@
  */
 
 #include "disttri.h"
+#include <random>
 
 namespace math
 {
+  //Reverse triangular distribution, from probability to x value
+  double dist_tri_rnd ( double a, double b, double c )
+  {
+    //Get Random number between 0 and 1
+    double prob = (rand() % 1000000) / 1000000;
+
+    //Calculate the position of the probability in the triangle
+
+    //Return the x coordinate as the given value
+
+    return 0;
+  }
 
 } /* namespace math */
--- math/disttri.h
+++ math/disttri.h
@@ -12,10 +12,10 @@
 
 //TODO : Calculate the x value of a given a, b, c points and probability
 //TODO : Calculate the probability of a point at x given a, b, c points
-
+//TODO : give better names to this file
 namespace math
 {
-
+  double dist_tri_rnd ( double a, double b, double c );
 
 } /* namespace math */
 
