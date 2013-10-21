f635a88605b1127d1691f0100ea9f76eafa075ae
David Barnett <david@barnett.net.nz>
2013-10-21 17:46:06 +1300

    Point Update * Moved Point class into maths folder and namespace + + and - operators + Doxygen comments

---
+++ maths/Point.h
@@ -0,0 +1,40 @@
+/*
+ * Point.h
+ *
+ *  Created on: 8/10/2013
+ *      Author: drb
+ */
+
+#ifndef POINT_H_
+#define POINT_H_
+
+// TODO : Add operators ( += , -= , == )
+#include <string>
+
+namespace maths {
+
+    class Point {
+public:
+    Point();
+    Point(const Point& p);
+    Point(double xy);
+    Point(double x, double y);
+
+    virtual ~Point();
+
+    //Operators
+
+    Point operator+(const Point&) const;
+    Point operator-(const Point&) const;
+
+
+    //To String Function
+    std::string str();
+    std::string str(char prefix, char mid , char postfix);
+
+    double x , y;
+};
+
+} /* namespace etc */
+
+#endif /* POINT_H_ */
