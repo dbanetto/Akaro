4ec8fa3ef9302bc778cefc84919f5b1417d014a9
David Barnett <david@barnett.net.nz>
2013-10-27 14:47:45 +1300

    Path Separator + cross platform path separators

--- io/file.h
+++ io/file.h
@@ -9,8 +9,21 @@
 #define FILE_H_
 
 #include <string>
-
+#include "../etc/env.h"
 // TODO : Add some SDL file operations
+
+#if __GNUC__
+#include <SDL2/SDL_filesystem.h>
+#else
+#include "SDL_filesystem.h"
+#endif
+
+
+#ifdef __WINDOWS__
+#define PATH_SEP '\\'
+#else
+#define PATH_SEP '/'
+#endif
 
 namespace IO {
     bool fileExists(const std::string& filename);
