a6a023cccba2453b17708d6cb777bdc4f9c7773c
David Barnett <david@barnett.net.nz>
2013-12-24 15:20:37 +1300

    maths/rect update + const & versions of functions

--- src/maths/rect.cpp
+++ src/maths/rect.cpp
@@ -28,6 +28,24 @@ namespace maths
       }
   }
 
+  bool isRectTouching (const SDL_Rect &aRect, const SDL_Rect &bRect )
+  {
+      /*
+        * Point's for rectangle (x,y) , (x+w,y) , (x+w,y+h) , (x,y+h)
+        * Checks if the points are in the bounds of each other
+        * Pointers to SDL_rect's are used for faster(?) usage of properties
+        */
+        if (aRect.x < (bRect.x + bRect.w) && (aRect.x + aRect.w) > bRect.x &&
+                aRect.y < (bRect.y + bRect.h) && (aRect.y + aRect.h) > bRect.y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+  }
+
   void rectSubtract (SDL_Rect* rect, SDL_Point pt )
   {
       rect->x -= pt.x;
@@ -44,6 +62,19 @@ namespace maths
   {
       if ( small->x > big->x && small->y > big->y
               && (small->x + small->w) < (big->x + big->w) && (small->y + small->h) < (big->y + big->h) )
+      {
+          return true;
+      }
+      else
+      {
+          return false;
+      }
+  }
+
+  bool isWholeRectInside (const SDL_Rect &small, const SDL_Rect &big )
+  {
+      if ( small.x > big.x && small.y > big.y
+              && (small.x + small.w) < (big.x + big.w) && (small.y + small.h) < (big.y + big.h) )
       {
           return true;
       }
